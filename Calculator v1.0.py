f8a2c4b5f8617eda193b4fd71f913c27880bd0d9
IvanHedz <ivan.gn03@gmail.com>
2020-10-16 20:46:41 +0300

    Pow functionaliny add.

--- Calculator v1.0.py
+++ Calculator v1.0.py
@@ -1,3 +1,4 @@
+from math import pow
 a = float(input("Enter the first number:"))
 op = input("Input the operation:")
 b = float(input("Enter the second number:"))
@@ -10,4 +11,6 @@ elif op == "-":
     result = a-b
 elif op == "*":
     result = a*b
+elif op == "pow":
+    result = pow(a,b)
 print("Result: {:.2f}".format(result))
